347ae54cfd3aca870939828c648945eca3f4e8db
nandhakumar2611 <101786839+nandhakumar2611@users.noreply.github.com>
2023-01-30 10:46:48 +0530

    Update AppFooter.js

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,10 +5,10 @@ const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <span className="ms-1">&copy; 2022 </span>
+        <span className="ms-1"> </span>
       </div>
       <div className="ms-auto">
-        <span className="me-1">Powered by React &amp; SpringBoot</span>
+        <span className="me-1">Powered by MindPLM</span>
       </div>
     </CFooter>
   )
